2545f6958c864fd24c85dcc3119015f18731c63b
pro <pro@prodeMacBook-Pro-2.local>
2023-06-16 18:16:49 +0600

    functionality project

---
+++ concepts.py
@@ -0,0 +1,29 @@
+"""
+1. Inheritance
+2. encapsulation
+3. polymorphism
+4. abstraction
+------------------
+use case
+classes
+1. User: base class(ABC)
+2. customer(User)
+3. Employee(User)
+4. Chef(Employee)
+5. Manager(Employee)
+6. Cleaner(Employee)
+7. Supplier(User)
+8. Marketer(User)
+
+1.FoodItem(ABC)
+2. Pizza(FoodItem)
+3. Burger(FoodItem)
+4. Drinks(FoodItem)
+5. Juice(FoodItem)
+6. Salad(FoodItem)
+
+1. Order
+2. Restaurant
+
+
+"""
