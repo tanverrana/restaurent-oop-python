fe5a00ce74a1bd1d567d68d06916496a265656f1
pro <pro@prodeMacBook-Pro-2.local>
2023-06-17 10:47:06 +0600

    add pizza item

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from menu import Pizza, Burger, Drinks, Menu
+
+
 def main():
-    print('Run this program')
+    menu = Menu()
+    pizza_1 = Pizza('chicken pizza', 600, 'large', ['chicken', 'onion'])
+    menu.add_menu_item('pizza', pizza_1)
+    pizza_2 = Pizza('Beef pizza', 600, 'large', ['Beef', 'onion'])
+    menu.add_menu_item('pizza', pizza_2)
+    pizza_3 = Pizza('Vegetable Pizza', 500, 'Medium',
+                    ['Vegetable', 'Onion', 'oil'])
+    menu.add_menu_item('pizza', pizza_3)
+
+    # add burger to the menu
 
 
 if __name__ == '__main__':
